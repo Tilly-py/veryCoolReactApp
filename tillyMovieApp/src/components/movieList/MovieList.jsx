89cbf6daa9df42c0d89e77d1716ec1ada5fcc1ee
Tilly-py <alex.s.tilly@outlook.com>
2026-05-17 10:24:04 +0200

    Byt ut "index" mot "movie.title" för lättare sortering

--- tillyMovieApp/src/components/movieList/MovieList.jsx
+++ tillyMovieApp/src/components/movieList/MovieList.jsx
@@ -5,10 +5,10 @@ const MovieList = ({ movies }) => {
     <div className="movie-container">
       <h2>Dina Filmer</h2>
       <div className="movie-list">
-        {movies.map((movie, index) => (
-          <div className="movie-list-item" key={index}>
+        {movies.map((movie) => (
+          <div className="movie-list-item" key={movie.title}>
             <div className="movie-content">
-              {movie.title}
+              <h3>{movie.title}</h3>
               <div className="movie-images">
                 {[...Array(Number(movie.rating))].map((_, starIndex) => (
                   <img key={starIndex} src="src/assets/star.png" alt="Rating" />
